563b38da186bdcaca4e20d11282517564ebe1311
c1027525Newcastle <c1027525@newcastle.ac.uk>
2021-10-29 17:16:57 +0100

    Added the Main() function acting as an Interface and made the beginning of the class Contacts

---
+++ Main.py
@@ -0,0 +1,41 @@
+class Contacts:
+    def __init__(self, name, address, phone_num, birthday):
+        self.Name = name
+        self.Address = address
+        self.Phone_num = phone_num
+        self.Birthday = birthday
+
+def Main():
+    try:
+        choice = int(input("""What do you want to do:
+    1) Create a new contact
+    2) Search for a contact
+    3) Retrieve all contacts and their details
+    Answer:"""))
+        print(choice)
+
+        if choice == 1:
+            print("Your choice was one")
+
+        elif choice == 2:
+            choice2 = int(input("""How do you want to search for the contact?
+    By looking for the:
+    1)Name
+    2)Phone number
+    3)Address
+    4)Birthday
+    Answer"""))
+            print(choice2)
+
+        elif choice == 3:
+            print("Your choice was 3")
+
+        else:
+            print("You can oly choose from the 3 options")
+            Main()  #Should find a better way to loop it
+
+    except:
+        print("Please choose one by inserting an integer")
+        Main()
+
+Main()
