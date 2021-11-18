63454f5fd4dbbc148cbc49f4d5636101106aeeba
c1027525Newcastle <c1027525@newcastle.ac.uk>
2021-11-18 17:02:59 +0000

    Half of the change_detail_contact function is done

--- Main.py
+++ Main.py
@@ -5,24 +5,25 @@ class Contacts:
         self.Phone_num = phone_num
         self.Birthday = birthday
 
-    def set_name(self, name): #still need to work on this
+    def set_name(self, name):  # still need to work on this
         if isinstance(name, str):
             self.name = name
         else:
             raise TypeError("Name should be string")
 
-#def create_new_contact():
+# def create_new_contact():
+
 
 def search_for_contact(choice2):
-    #Still need to change the method where it calls the main() as it's stopping at the first found element/no duplicates are found
+    # Still need to change the method where it calls the main() as it's stopping at the first found element/no duplicates are found
     if choice2 == 1:
         name = input("Name?")
         with open("Contacts.txt", 'r') as MyFile:
             line = MyFile.readline()
             while line != "":
-                newline = line.split(",")
+                newline = line.rstrip("\n").split(",")
                 if newline[0] == name:
-                    print("This is the contact with the name", name +":")
+                    print("This is the contact with the name", name + ":")
                     print(newline)
                     main()
                 line = MyFile.readline()
@@ -32,9 +33,9 @@ def search_for_contact(choice2):
         with open("Contacts.txt", 'r') as MyFile:
             line = MyFile.readline()
             while line != "":
-                newline = line.split(",")
+                newline = line.rstrip("\n").split(",")
                 if newline[2] == phone_num:
-                    print("This is the contact with the phone number", phone_num +":")
+                    print("This is the contact with the phone number", phone_num + ":")
                     print(newline)
                     main()
                 line = MyFile.readline()
@@ -44,9 +45,9 @@ def search_for_contact(choice2):
         with open("Contacts.txt", 'r') as MyFile:
             line = MyFile.readline()
             while line != "":
-                newline = line.rstrip("\n").split(",") #need to strip \n########
+                newline = line.rstrip("\n").split(",")
                 if newline[3] == birthday:
-                    print("This is the contact with the birthday", birthday +":")
+                    print("This is the contact with the birthday", birthday + ":")
                     print(newline)
                     main()
                 line = MyFile.readline()
@@ -56,12 +57,13 @@ def search_for_contact(choice2):
         with open("Contacts.txt", 'r') as MyFile:
             line = MyFile.readline()
             while line != "":
-                newline = line.split(",")
+                newline = line.rstrip("\n").split(",")
                 if newline[1] == address:
-                    print("This is the contact with the address", address +":")
+                    print("This is the contact with the address", address + ":")
                     print(newline)
                     main()
                 line = MyFile.readline()
+
 
 def retrieve_all_contacts():
     with open("Contacts.txt", 'r') as MyFile:
@@ -70,6 +72,41 @@ def retrieve_all_contacts():
         while line != "":
             print(line.rstrip("\n"))
             line = MyFile.readline()
+
+
+def change_detail_contact():
+    name = input("What is the name of the contact you want some details to be changed?")
+    with open("Contacts.txt", 'r') as MyFile:
+        line = MyFile.readline()
+        count = 0
+        while line != "":
+            newline = line.rstrip("\n").split(",")
+            if newline[0] == name:
+                print("These are the details of the contact\n", newline)
+                line_number = count
+                line_to_be_changed = newline
+            count = count + 1
+            line = MyFile.readline()
+    myfile = open("Contacts.txt", 'r')
+    lines = myfile.readlines()
+    choice4 = int(input("""What do you want to change?
+    1) Name
+    2) Address
+    3) Birthday
+    4) Telephone"""))
+    if choice4 == 1:
+        new_name = input("Enter the new name:")
+        lines[line_number] = new_name + "," + line_to_be_changed[1] + "," + line_to_be_changed[2] + "," + line_to_be_changed[3] + "\n"
+        myfile.close
+    elif choice4 == 2:
+        new_address = input("Enter the new address")
+        lines[line_number] = line_to_be_changed[0] + "," + new_address + "," + line_to_be_changed[2] + "," + line_to_be_changed[3] + "\n"
+        myfile.close
+
+    myfile = open("Contacts.txt", 'w')
+    myfile.writelines(lines)
+    myfile.close
+
 
 def main():
     try:
@@ -102,7 +139,7 @@ def main():
             retrieve_all_contacts()
 
         elif choice == 4:
-            print("Your choice was 4")
+            change_detail_contact()
 
         elif choice == 5:
             print("Closing App")
@@ -114,5 +151,6 @@ def main():
     except:
         print("Please choose one by inserting an integer")
         main()
+
 
 main()
